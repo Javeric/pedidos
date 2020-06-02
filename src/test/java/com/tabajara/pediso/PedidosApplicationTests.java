1bf7944d9509a80f379f22ea81ecd62a5441ad47
Eric <ericrangel13@hotmail.com>
2020-06-02 05:50:46 -0300

    Project Created

---
+++ src/test/java/com/tabajara/pediso/PedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tabajara.pediso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
